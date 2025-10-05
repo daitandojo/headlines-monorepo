76552334ab66e877ccc1020affddfbb2fb4d3711
Daitan Dojo <haelpers@gmail.com>
2025-10-05 11:03:47 +0100

    Simpler postcss

--- apps/client/postcss.config.js
+++ apps/client/postcss.config.js
@@ -1,10 +1,5 @@
-// File: apps/client/postcss.config.js (THE FINAL VERSION)
 module.exports = {
   plugins: {
-    // This tells PostCSS to look for a tailwind.config.js file
-    // and process the CSS with it. It should find the one
-    // in the same directory.
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
